22425a947efa5ca448e283db178a9867db0d5bf3
Steven Lee <kynrai@gmail.com>
2019-03-17 15:03:23 +0000

    Add BatchWriteWithContext

--- batch_write.go
+++ batch_write.go
@@ -1,7 +1,10 @@
 package dynamodbx
 
 import (
+	"context"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
@@ -35,6 +38,78 @@ func BatchWriteItem(client *dynamodb.DynamoDB, input *dynamodb.BatchWriteItemInp
 			chunkSize = 25
 
 			out, err := client.BatchWriteItem(batch)
+			if err != nil {
+				return nil, err
+			}
+			fOut.ConsumedCapacity = append(fOut.ConsumedCapacity, out.ConsumedCapacity...)
+			for k, v := range out.ItemCollectionMetrics {
+				fOut.ItemCollectionMetrics[k] = append(fOut.ItemCollectionMetrics[k], v...)
+			}
+
+			// If we have no unprocessed items we make sure the next iteration has the full chunkSize
+			if len(out.UnprocessedItems) == 0 {
+				chunkSize = 25
+				continue
+			}
+
+			// We have unprocessed items at this point, we deduct the number from the chunkSize to add
+			// them both next iteration
+			chunkSize -= len(out.UnprocessedItems)
+		}
+	}
+	// Sum up multiple ConsumedCapacity structs
+	sum := make(map[string]*dynamodb.ConsumedCapacity)
+	for _, v := range fOut.ConsumedCapacity {
+		if _, ok := sum[*v.TableName]; !ok {
+			sum[*v.TableName] = &dynamodb.ConsumedCapacity{
+				CapacityUnits: v.CapacityUnits,
+			}
+			continue
+		}
+		*sum[*v.TableName].CapacityUnits += *v.CapacityUnits
+	}
+	sliceCap := make([]*dynamodb.ConsumedCapacity, 0, len(sum))
+	for k, v := range sum {
+		sliceCap = append(sliceCap, &dynamodb.ConsumedCapacity{
+			TableName:     aws.String(k),
+			CapacityUnits: v.CapacityUnits,
+		})
+	}
+	fOut.ConsumedCapacity = sliceCap
+	return fOut, nil
+}
+
+// BatchWriteItemWithContext is a wrapper around the aws-sdk-go dynamodb.BatchWriteItemWithContext. It will attemptt to
+// automatically breakup batch writes into smaller batches so that inputs larger tthan 25 items
+// can be easily processed. Use it as a drop in replacement for the existting BatchWriteItem command
+// but with the dynamodb client supplied as the first paramter.
+// A context and request options can be provided to and will be passed to the underlying aws-sdk-go calls
+func BatchWriteItemWithContext(ctx context.Context, client *dynamodb.DynamoDB, input *dynamodb.BatchWriteItemInput, opts ...request.Option) (*dynamodb.BatchWriteItemOutput, error) {
+	fOut := &dynamodb.BatchWriteItemOutput{}
+	for tableName, items := range input.RequestItems {
+		var unprocessedItems map[string][]*dynamodb.WriteRequest
+		chunkSize := 25
+		for i := 0; i < len(items); i += chunkSize {
+			end := i + chunkSize
+			if end > len(items) {
+				end = len(items)
+			}
+			batch := &dynamodb.BatchWriteItemInput{
+				ReturnConsumedCapacity:      input.ReturnConsumedCapacity,
+				ReturnItemCollectionMetrics: input.ReturnItemCollectionMetrics,
+				RequestItems: map[string][]*dynamodb.WriteRequest{
+					tableName: items[i:end],
+				},
+			}
+			if len(unprocessedItems) > 0 {
+				for k, v := range unprocessedItems {
+					batch.RequestItems[k] = append(batch.RequestItems[k], v...)
+				}
+			}
+			// after adding all the unprocessed items we reset the chunkSize back to 25
+			chunkSize = 25
+
+			out, err := client.BatchWriteItemWithContext(ctx, batch, opts...)
 			if err != nil {
 				return nil, err
 			}
--- batch_write_test.go
+++ batch_write_test.go
@@ -1,13 +1,15 @@
 package dynamodbx_test
 
 import (
+	"context"
 	"reflect"
 	"strconv"
+	"strings"
 	"testing"
-
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/kylelemons/godebug/pretty"
@@ -120,6 +122,137 @@ func TestBatchWriteItem(t *testing.T) {
 				RequestItems:                req,
 			})
 			if err != nil {
+				t.Fatal(err)
+			}
+			if tc.expect != nil && !reflect.DeepEqual(out, tc.expect) {
+				t.Fatalf(pretty.Compare(out, tc.expect))
+			}
+		})
+	}
+}
+
+func TestBatchWriteItemWithContext(t *testing.T) {
+	t.Parallel()
+	type TestData struct {
+		S string
+	}
+	testDataSet := func(count int) []*TestData {
+		data := make([]*TestData, count)
+		for i := 0; i < count; i++ {
+			data[i] = &TestData{
+				S: strconv.Itoa(i),
+			}
+		}
+		return data
+	}
+	ctxDead, cfunc := context.WithDeadline(context.Background(), time.Now().Add(-time.Minute*1))
+	defer cfunc()
+	for _, tc := range []struct {
+		ctx     context.Context
+		name    string
+		table   string
+		input   interface{}
+		expect  *dynamodb.BatchWriteItemOutput
+		errText string
+	}{
+		{
+			name:  "insert 2 items",
+			table: "test1",
+			input: testDataSet(2),
+			expect: &dynamodb.BatchWriteItemOutput{
+				ConsumedCapacity: []*dynamodb.ConsumedCapacity{
+					{
+						CapacityUnits: aws.Float64(2),
+						TableName:     aws.String("test1"),
+					},
+				},
+			},
+			ctx: context.Background(),
+		},
+		{
+			name:  "insert 115 items",
+			table: "testSet",
+			input: testDataSet(115),
+			expect: &dynamodb.BatchWriteItemOutput{
+				ConsumedCapacity: []*dynamodb.ConsumedCapacity{
+					{
+						CapacityUnits: aws.Float64(115),
+						TableName:     aws.String("testSet"),
+					},
+				},
+			},
+			ctx: context.Background(),
+		},
+		{
+			name:  "insert 1000 items",
+			table: "testLargeSet",
+			input: testDataSet(1000),
+			expect: &dynamodb.BatchWriteItemOutput{
+				ConsumedCapacity: []*dynamodb.ConsumedCapacity{
+					{
+						CapacityUnits: aws.Float64(1000),
+						TableName:     aws.String("testLargeSet"),
+					},
+				},
+			},
+			ctx: context.Background(),
+		},
+		{
+			name:    "context timeout",
+			table:   "testLargeSetWithCtx",
+			input:   testDataSet(1000),
+			ctx:     ctxDead,
+			errText: "context deadline exceeded",
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			// Must have a local dynamodb running
+			ddb := dynamodb.New(
+				session.Must(session.NewSession(
+					&aws.Config{
+						Region:      aws.String("eu-west-1"),
+						Endpoint:    aws.String("http://localhost:8000"),
+						Credentials: credentials.NewStaticCredentials("foo", "bar", "foobar"),
+					},
+				)),
+			)
+			_, err := dynamodbx.CreateTableSyncWithContext(tc.ctx, ddb, &dynamodb.CreateTableInput{
+				TableName:   aws.String(tc.table),
+				BillingMode: aws.String(dynamodb.BillingModePayPerRequest),
+				AttributeDefinitions: []*dynamodb.AttributeDefinition{
+					{
+						AttributeName: aws.String("S"),
+						AttributeType: aws.String(dynamodb.ScalarAttributeTypeS),
+					},
+				},
+				KeySchema: []*dynamodb.KeySchemaElement{
+					{
+						AttributeName: aws.String("S"),
+						KeyType:       aws.String(dynamodb.KeyTypeHash),
+					},
+				},
+			})
+			defer ddb.DeleteTableWithContext(tc.ctx, &dynamodb.DeleteTableInput{
+				TableName: aws.String(tc.table),
+			})
+			if tc.errText == "" && err != nil {
+				t.Fatal(err)
+			}
+			if tc.errText != "" && !strings.Contains(err.Error(), tc.errText) {
+				t.Fatalf("expected error %v got %v", tc.errText, err.Error())
+			}
+			req, err := dynamodbx.BatchPutRequest(tc.table, tc.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			out, err := dynamodbx.BatchWriteItemWithContext(tc.ctx, ddb, &dynamodb.BatchWriteItemInput{
+				ReturnConsumedCapacity:      aws.String("TOTAL"),
+				ReturnItemCollectionMetrics: aws.String("SIZE"),
+				RequestItems:                req,
+			})
+			if tc.errText == "" && err != nil {
 				t.Fatal(err)
 			}
 			if tc.expect != nil && !reflect.DeepEqual(out, tc.expect) {
